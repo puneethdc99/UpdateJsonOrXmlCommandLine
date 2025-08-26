6c84b99c6fbe605710397bfa99175458cffffa4b
puneethdc99 <58505120+puneethdc99@users.noreply.github.com>
2025-08-26 16:04:32 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using UpdateJsonValue;
 
 Console.WriteLine("Update Prop");
@@ -6,8 +6,11 @@ Console.WriteLine("Update Prop");
 
 //Updating appsettings.json
 //DataUpdater4.UpdateValue("appsettings.json", "File:Path", "c:\\puneeth");
-DataUpdater4.UpdateValue("appsettings.json", "Users:1:Name", "puneeth");
-//DataUpdater4.UpdateValue("appsettings.json", "Modules:0:ExportFormats:1:0", "puneeth");
+DataUpdater5.UpdateValue("appsettings.json", "AppName", "puneeth");
+//DataUpdater4.UpdateValue("appsettings.json", "Modules:0:ExportFormats:1", "puneeth");
+
+//DataUpdater5.UpdateValue("appsettings.json", "Modules:0:Settings:ExportFormats:2", "puneeth");
+
 
 
 Console.ReadLine();
